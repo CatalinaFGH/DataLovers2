d931a581dc0fbfffffceb1ab0885947f1290b14c
unknown <catalinagonzalez1410@gmail.com>
2021-04-25 20:56:03 -0300

    maquetada la segunda pantalla

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import { example } from './data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+
+//Variables globales para cambiar de pantalla
+const welcomePage = document.getElementById("sectionContainer");
+const charactersPage = document.getElementById("charactersPage");
+//Función para pasar a la segunda pantalla
+const changePage = document.getElementById("enterButton");
+      changePage.addEventListener("click", () => {
+          console.log("GO")
+        welcomePage.style.display = "none";
+        charactersPage.style.display = "block";
+});
