b1b74f0ccb3f4a8d5f67fa2edc11a02727d1b454
unknown <catalinagonzalez1410@gmail.com>
2021-05-06 16:55:04 -0300

    agregada la función de orden alfabético. Terminada la funcionalidad de filtrado

--- src/data.js
+++ src/data.js
@@ -1,13 +1,3 @@
-// estas funciones son de ejemplo
-
-// export const example = () => {
-//   return 'example';
-// };
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
-
 const functions = {
 
   toFilter(arrayrickymorty, elementName, filterValue){
@@ -18,10 +8,43 @@ const functions = {
   search(arrayrickymorty, characterName){
     let arrayrickymortyTwo = arrayrickymorty.filter(element => element.name.toLowerCase().includes(characterName));
     return arrayrickymortyTwo;
+  },
+
+  orderAZ(arrayrickymorty) {
+    let orderedInfo = arrayrickymorty.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      { 
+        return -1;
+      }
+    });
+    return orderedInfo;
+  },
+
+  orderZA(arrayrickymorty) {
+    let orderedInfo = arrayrickymorty.sort(function (a, b) {
+      if (a.name < b.name){
+        return 1;
+      }
+      {
+        return -1;
+      }
+    });
+    return orderedInfo;
+  },
+
+  orderDefault(arrayrickymorty) {
+    let orderedInfo = arrayrickymorty.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      {
+        return -1;
+      }
+    });
+    return orderedInfo;
   }
-
-
-
 }
 
 export default functions;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-// import { example } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 import functions from './data.js'
-// console.log(example, data);
 
 //Variables globales para cambiar de pantalla
 const welcomePage = document.getElementById("sectionContainer");
@@ -14,30 +12,9 @@ const changePage = document.getElementById("enterButton");
         charactersPage.style.display = "block";
 });
 
-//Función para crear las tarjetas de los personajes
-// let info = data.results;
+// Guardamos los resultados de la data del js en una variable
 let seriesData = data.results;
-// let dataContainer = document.getElementById("dataContainer");
-
-// window.addEventListener("load", addAllCharacters(info));
-// function addAllCharacters(info) {
-//   dataContainer.innerHTML = "";
-//   for (let i = 0; i < info.length; i++) {
-//     let card = document.createElement("DIV");
-//     let image = document.createElement("IMG");
-//     let name = document.createElement("P");
-//     card.setAttribute("class", "card");
-//     card.setAttribute("id", "card");
-//     image.setAttribute("src", info[i].image);
-//     image.setAttribute("class", "cardImage");
-//     name.setAttribute("class", "characterName");
-//     name.innerHTML = info[i].name;
-//     dataContainer.appendChild(card);
-//     card.appendChild(image);
-//     card.appendChild(name);
-//   }
-// }
-// window.addEventListener("load", printCharacters(seriesData));
+//Función para crear las tarjetas de los personajes
 function printCharacters(data){
   let list = "";
   for(let i = 0; i < data.length; i++){
@@ -62,38 +39,38 @@ function printCharacters(data){
 }
 document.getElementById("dataContainer").innerHTML = printCharacters(seriesData);
 
-
 //Función para filtrar la data
 function selectFunction() {
   let TheFiltersArray = seriesData;
   let filterGender = document.getElementById("selectGender").value;
   let filterSpecies = document.getElementById("selectSpecies").value;
-  // let orderAZ = document.getElementById("selectOrderAZ").value;
+  let orderAZ = document.getElementById("selectOrderAZ").value;
   let searchBar = document.getElementById("searchInput").value;
   //si el filtro no viene vacío se ejecuta la función filtrar, que devolverá el arreglo con los datos filtrados
+  if (searchBar != "") {
+    TheFiltersArray = functions.search(TheFiltersArray, searchBar);
+  }
   if (filterGender != "") {
     TheFiltersArray = functions.toFilter(TheFiltersArray, "gender", filterGender);
   }
   if (filterSpecies != "") {
     TheFiltersArray = functions.toFilter(TheFiltersArray, "species", filterSpecies);
   }
-  // if (orderAZ != "") {
-  //   TheFiltersArray = toOrderAZ(TheFiltersArray, orderAZ);
-  // }
-  if (searchBar != "") {
-    TheFiltersArray = functions.search(TheFiltersArray, searchBar);
+  //para el filtro de orden alfabético debemos comparar el valor del option del select para ejecutar la función filtrar
+  if (orderAZ == "1") {
+    TheFiltersArray = functions.orderAZ(TheFiltersArray, orderAZ);
   }
-
+  if (orderAZ == "2") {
+    TheFiltersArray = functions.orderZA(TheFiltersArray, orderAZ);
+  }
+  if (orderAZ == "default") {
+    TheFiltersArray = functions.orderDefault(TheFiltersArray, orderAZ);
+  }
   //Pintamos las cartas otra vez, pero con la data filtrada
   document.getElementById("dataContainer").innerHTML = printCharacters(TheFiltersArray);
 }
 selectFunction();
 document.getElementById("selectGender").addEventListener("change", selectFunction);
 document.getElementById("selectSpecies").addEventListener("change", selectFunction);
-// document.getElementById("selectOrderAZ").addEventListener("change", selectFunction);
+document.getElementById("selectOrderAZ").addEventListener("change", selectFunction);
 document.getElementById("searchInput").addEventListener("keyup", selectFunction);
-
-
-
-
-
